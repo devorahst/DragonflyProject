6ce2fe82028ee6b7ceb3610060029c9449d7b03b
Mikaela Gardner <mikaelagardner24@gmail.com>
2022-03-04 15:17:28 -0700

    Update fig2_plot.R

--- fig2_plot.R
+++ fig2_plot.R
@@ -6,5 +6,8 @@ lvls = c("OO", "OE", "EE", "EO")
 Fig2 = read_csv("fig2_summary.csv")%>%
   mutate(Comparison = factor(Comparison, levels = lvls))
 
-ggplot(Fig2, aes(x = Comparison, y = Percent_Recovery)) + 
-  geom_boxplot() + theme_bw() + xlab("Comparisons") + ylab("Percent Recovery")
+ggplot(Fig2, mapping = aes(x = Comparison, y = Percent_Recovery)) + 
+  geom_boxplot() + theme_bw() + geom_jitter(color="black", size=0.5, alpha=0.9) +
+  xlab("Comparisons") + ylab("Percent Recovery") + ggtitle("Figure 2 Example") +
+  theme(plot.title = element_text(hjust = 0.5))
+
