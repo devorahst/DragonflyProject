c0f35867dc198c152209bbf14e0fd64e7ea1064a
Mikaela Gardner <mikaelagardner24@gmail.com>
2022-03-05 11:58:28 -0700

    Update fig2_plot.R
    Added color and legend

--- fig2_plot.R
+++ fig2_plot.R
@@ -1,13 +1,16 @@
 library(tidyverse)
 library(ggplot2)
 
-lvls = c("OO", "OE", "EE", "EO")
-
+c_lvls = c("O/O", "E/E", "O/E", "E/O")
+ct_lvls = c("Matched", "Mismatched")
 Fig2 = read_csv("fig2_summary.csv")%>%
-  mutate(Comparison = factor(Comparison, levels = lvls))
+  mutate(Comparison = factor(Comparison, levels = c_lvls))
+  mutate(Comparison_Type = factor(Comparison_Type, levels = ct_lvls))
 
-ggplot(Fig2, mapping = aes(x = Comparison, y = Percent_Recovery)) + 
+ggplot(Fig2, mapping = aes(x = Comparison, y = Recovered_loci, fill = Comparison_Type)) + 
   geom_boxplot() + theme_bw() + geom_jitter(color="black", size=0.5, alpha=0.9) +
-  xlab("Comparisons") + ylab("Percent Recovery") + ggtitle("Figure 2 Example") +
+  labs(title="Recovered Loci vs AHE Probeset Combinations",x="AHE data/Reference loci", 
+       y="Recovered loci", fill="Comparison Type")
   theme(plot.title = element_text(hjust = 0.5))
+
 
