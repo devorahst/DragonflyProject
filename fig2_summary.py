ae2d3223b13687b3005c4434e2bac7fbbf10c542
Mikaela Gardner <mikaelagardner24@gmail.com>
2022-03-05 11:55:53 -0700

    Update fig2_summary.py
    Made it more user friendly (parse through all 4 directories, determines Comparison) and added new column called Comparison_Type.

--- fig2_summary.py
+++ fig2_summary.py
@@ -1,19 +1,31 @@
 import os
 import csv
 from_dir = '.'
-comparison = 'EE' #EE EO OO OE
+
 
 with open ('fig2_summary.csv','w', newline = '' ) as csvfile:
         summarywriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        summarywriter.writerow(['Comparison', 'Species', 'Recovered_loci'])
-        files_in_directory = os.listdir(from_dir)
-        filtered_files = [file for file in files_in_directory if file.endswith(".fasta")]
-        filtered_files.sort()
-        for filename in filtered_files:
-                if filename.find("ORTHO") != -1:
-                        species_id = filename[0:8]
-                        filepath = os.path.join(from_dir, filename)#complimentary ORTHO.fasta
-                        with open(filepath, 'r') as ORTHO_file:
-                                ortho = ORTHO_file.read()
-                                recovered_loci = ortho.count('>L')#count # loci
-                                summarywriter.writerow([comparison, species_id, str(recovered_loci)])
+        summarywriter.writerow(['Comparison', 'Comparison_Type', 'Species', 'Recovered_loci'])
+        directories_in_directory = os.listdir(from_dir)
+        for directory in directories_in_directory:
+                if directory.find('ephem') != -1 or directory.find('odonata') != -1:
+                        comparison = directory[0] + '/' + directory.split('_', 2)[-1][0]
+                        comparison = comparison.upper()
+                        if comparison[0] == comparison[2]:
+                                comparison_type = 'Matched'
+                        else:
+                                comparison_type = 'Mismatched'
+                        files_in_directory = os.listdir(directory)
+                        files_in_directory.sort()
+                        for filename in files_in_directory:
+                                if filename.find('ORTHO') != -1 and filename.find('.fasta') != -1:
+                                        species_id = filename[0:8]
+                                        filepath = os.path.join(from_dir, directory, filename)#complimentary ORTHO.fasta
+                                        try:
+                                                with open(filepath, 'r') as ORTHO_file:
+                                                        ortho = ORTHO_file.read()
+                                                        recovered_loci = ortho.count('>L')#count # loci
+                                                        summarywriter.writerow([comparison, comparison_type, species_id, str(recovered_loci)])
+                                        except IOError as e:# ignore directiories that don't contain the fasta
+                                                if e.errno != errno.ENOENT:#raise exception if dif from "file or directory doesn't exist"
+                                                        raise
